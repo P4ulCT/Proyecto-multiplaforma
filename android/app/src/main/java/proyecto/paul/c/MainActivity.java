35c993aef1498cee27284a67317cd4daceb1b506
P4ulCT <paul.cadena@gmail.com>
2026-03-04 08:50:27 -0500

    Inicializacion del proyecto

---
+++ android/app/src/main/java/proyecto/paul/c/MainActivity.java
@@ -0,0 +1,5 @@
+package proyecto.paul.c;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'proyecto.paul.c',
+  appName: 'proyecto-paul-c',
+  webDir: 'dist'
+};
+
+export default config;
